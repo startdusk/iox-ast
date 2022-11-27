4607ab317c2b5fedb8ec062bb7bbe025ce1209d3
startdusk <522383345@qq.com>
2022-11-27 10:06:25 +0800

    refactor🎨:  update

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
+use std::env::args;
 use std::fs::read_to_string;
-use std::io::{stdin, stdout, Write};
-use std::{env::args, process::exit};
+use std::io::{self, BufRead, Write};
+use std::process;
 
 fn main() {
     let args: Vec<String> = args().collect();
     println!("args: {:?}, len={}", args, args.len());
 
     match args.len() {
-        2 => run_file(&args[1]),
-        len if len > 2 => {
-            println!("Usage: rlox [script]");
-            exit(64)
+        1 => run_prompt(),
+        2 => {
+            run_file(&args[1]);
         }
-        _ => run_prompt(),
+        _ => {
+            println!("Usage: rlox [script]");
+            process::exit(64)
+        }
     }
 }
 
@@ -21,30 +24,23 @@ fn run_file(path: &String) {
     run(source)
 }
 
-fn run(_source: String) {}
+fn run(source: String) {
+    println!("{}", source)
+}
 
 fn run_prompt() {
-    loop {
-        print!("> ");
-        stdout().flush().unwrap();
-        let mut input = String::new();
-        match stdin().read_line(&mut input) {
-            Ok(_) => {
-                let input = input.trim();
-                match input {
-                    "exit" => {
-                        print!("Bye~");
-                        return;
-                    }
-                    _ => {
-                        println!("{input}");
-                    }
-                }
-            }
-            Err(error) => {
-                print!("error: {error}");
-                continue;
-            }
+    print!("> ");
+    let _ = io::stdout().flush();
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        let Ok(line) = line else {
+            break;
+        };
+        if line.is_empty() {
+            break;
         }
+        run(line);
+        print!("> ");
+        let _ = io::stdout().flush();
     }
 }
